0b43b103cfe2f3f197d546f38472bc58d7b0e9fd
DEBAPRASAD SHA <debaprasadsha98@gmail.com>
2026-07-03 22:22:13 +0530

    add

---
+++ add.java
@@ -0,0 +1,16 @@
+class add{
+    public static void main(String[] args){
+        System.out.println("The sum of the three numbers:");
+        int num1 = 3;
+        int num2 = 5;
+double num3 = 8.67;
+double sum = num1+num2+num3;
+System.out.println(sum);
+    }
+}
+// println = here ln is used for create a new line 
+// if we write a only print("xyz") here dosen't create any new line output is show in the same line
+// for ex- println("add numbers are: ")
+//          println("32")
+// output=    add numbers re:
+//            32   if only write print("") the output will be add numbers are: 32
