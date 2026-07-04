b7e32fe5b3fbcd843dd765ba9047f56972e8196d
DEBAPRASAD SHA <debaprasadsha98@gmail.com>
2026-07-04 22:15:48 +0530

    literals

---
+++ literals.java
@@ -0,0 +1,13 @@
+public class literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 344444444444L;
+        char ch = 'A';
+        float f1 = 4.5f;
+        double d1 = 3.22;
+        boolean a = true;
+        System.out.println("age");
+    }
+}
