a39d7af1dade4a3832a0857b4d7de541f877bee1
DEBAPRASAD SHA <debaprasadsha98@gmail.com>
2026-07-06 13:27:55 +0530

    operators

---
+++ ch2_operators.java
@@ -0,0 +1,14 @@
+public class ch2_operators {
+    public static void main(String[] args) {
+        int a = 4; //arithmatic operator
+     //   int b = 6 % a; //modulo operator
+    //assignment operator
+     int b = 8;
+     b += 3;
+        System.out.println(b);
+        System.out.println(2<=5); //comparasion opertor
+        //logical operatoer
+        System.out.println(8>5 && 8<2);
+        System.out.println(7>6 || 9>898);
+    }
+}
