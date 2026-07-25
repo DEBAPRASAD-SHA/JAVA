4e1dfe2bf5e555c3f3af3854e703d5d18744047c
DEBAPRASAD SHA <debaprasadsha98@gmail.com>
2026-07-25 22:15:17 +0530

    java project

---
+++ practise_set.java
@@ -0,0 +1,40 @@
+//Qusetion 1 sum of 3nos.
+
+import java.util.Scanner;
+
+public class practise_set {
+    public static void main(String[] args) {
+      //  int a = 10;
+     //   int b = 19;
+       // int c = 12;
+        //int sum = a+b+c;
+        //System.out.println(sum);
+
+
+//question 2 CGPA calculation
+// float sub1 = 55;
+// float sub2 = 45;
+// float sub3 = 57;
+// float cgpa = (sub1+sub2+sub3)/30;
+// System.out.println(cgpa);
+// 
+
+//Qusetion 3 name greet
+// System.out.println("What is your name");
+// Scanner sc = new Scanner(System.in);
+// String name = sc.next();
+// System.out.println("Hello " + name + " have a good day!");
+
+//Question 4 km to miles
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter the kilometers");
+// double km = sc.nextDouble();
+// double miles = km*0.621371;
+// System.out.println(miles+ " miles ");
+
+//Question 5 the number integer or not
+System.out.println("Enter the number");
+Scanner sc = new Scanner(System.in);
+System.out.println(sc.hasNextInt());
+}
+}
